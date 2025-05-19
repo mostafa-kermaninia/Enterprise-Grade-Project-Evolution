642b41c4befa7893cce43f95222eb64dab0eb1e8
Mostafa Kermaninia <mkermani1383@gmail.com>
2025-05-19 03:52:14 +0330

    Update SimpleLang.java

--- src/SimpleLang.java
+++ src/SimpleLang.java
@@ -22,7 +22,6 @@ public class SimpleLang {
         Program program = flParser.program().programRet;
         System.out.println();
 
-
         
         NameAnalyzer my_name_analyzer = new NameAnalyzer();
         my_name_analyzer.visit(program);
