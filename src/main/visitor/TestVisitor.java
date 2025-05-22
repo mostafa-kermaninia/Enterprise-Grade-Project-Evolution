1a339be5dbd418615243d247f9a7fb0abe7cc720
Mostafa Kermaninia <mkermani1383@gmail.com>
2025-05-22 04:45:14 +0330

    Update TestVisitor.java

--- src/main/visitor/TestVisitor.java
+++ src/main/visitor/TestVisitor.java
@@ -12,17 +12,6 @@ import main.ast.statement_DIR.*;
 import main.ast.expression_DIR.*;
 
 
-
-/*GOALs:
- *   1. print out scope changes each time a new scope starts
- *   2. print the identifier if it is initialized
- *   3. print the identifier if it is used
- *   4. print out the name of the function when it is defined
- *   5. print out the name of the function when it is used
- *
- * */
-
-
 public class TestVisitor extends Visitor<Void>{
     @Override
     public Void visit(Program program) {
